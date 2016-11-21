d1d18ebac331402f64c20806f3ecee027e7d127f
shareming7 <2395277764@qq.com>
2016-11-21 13:53:58 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
+var v1ApiRouter = express.Router();
 app.set('view engine','pug')
 app.set('views', './views');
-app.get('/v1', function (req, res) {
-	var page = req.query.page
-	var nperpage = req.query.nperpage
- res.render('index', { page: page, nperpage: nperpage,name:'#{user.name}',description:'#{user.description' })
+v1ApiRouter.get('/', function (req, res) {
+var page = req.query.page
+var nperpage = req.query.nperpage
+res.render('index', { page: page, nperpage: nperpage,name:name,description:description })
 })
+
+app.use('/v1',v1ApiRouter);
+
 app.listen(3000,function(){
 
 console.log('port 3000')

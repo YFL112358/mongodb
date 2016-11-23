f2e73ce1dc17c8f19253d3d144ccd9beb6853a13
shareming7 <2395277764@qq.com>
2016-11-23 15:53:15 +0800

    modified index.js

--- index.js
+++ index.js
@@ -7,48 +7,38 @@ var db;
 
 // Initialize connection once
 MongoClient.connect("mongodb://localhost:27017/mypractice", function(err, database) {
-  if(err) throw err;
+  if (err) throw err;
   db = database;
 });
-
 
 var v1ApiRouter = express.Router();
 app.set('view engine','pug')
 app.set('views', './views');
 v1ApiRouter.get('/', function (req, res) {
-		//进行数据库操作
+	//进行数据库操作
 	db.collection('item',{safe:true}, function(err, collection){
-	if(err){
-		console.log(err);
-	}else{
-	 var page = Number(req.query.page);
-	 var nperpage = Number(req.query.nperpage);
+		if (err) {
+			console.log(err);
+		} else {
+			var page = Number(req.query.page);//page 是第几页
+			var nperpage = Number(req.query.nperpage);//分页功能，nperpage 是一页里面的记录数
 
-	 console.log(page);
-	//分页功能，nperpage 是一页里面的记录数
-	//page 是第几页
-	//skip()跳了几条记录，limit(一页的记录数)		  
-	collection.find()
-	.skip((page-1)*nperpage) 
-	.limit(nperpage)
-  .toArray(function(err,results){
-		   console.log(results);
-			res.render('index', {results:results })
+			console.log("page : " + page);
 
-							              	});
-
-				db.close();
-						}
-				});
-			
-
-		});
-
+			collection.find()
+			.skip((page-1)*nperpage)//skip()跳了几条记录，limit(一页的记录数) 
+			.limit(nperpage)
+			.toArray(function(err,results){
+					console.log(results);
+					res.send({results:results })
+			});
+		}
+	});
+});
 
 app.use('/v1',v1ApiRouter);
 
 app.listen(3000,function(){
-
-console.log('port 3000')
+		console.log('port 3000')
 })
 

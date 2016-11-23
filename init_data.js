2a8ded323c5484d2b0a62f50f487c38230be7505
shareming7 <2395277764@qq.com>
2016-11-23 14:16:46 +0800

    Update init_data.js

--- init_data.js
+++ init_data.js
@@ -15,16 +15,16 @@ var  db = new mongodb.Db('mypractice', server, {safe:true});
       var a = getRandomString();
       console.log(a);
     //连接数据库
-			db.open(function(err, db){
-    if(!err){
+      db.open(function(err, db){
+      if(!err){
         console.log('connect db');
          db.collection('item',{safe:true}, function(err, collection){
              if(err){
                  console.log(err);
              }else{
-						//插入数据库之前清空数据库
-										collection.remove();
-						//插入100条记录
+		 //插入数据库之前清空数据库
+		    collection.remove();
+		//插入100条记录
                     for(var i = 0;i<100;i++){
                     collection.insert({ name: a+i, description: a+i,last_updated:new Date() }). then(console.log);}
 

d0b198772988b844178694e8aa0192de35d53b46
shareming7 <2395277764@qq.com>
2016-11-23 13:09:10 +0800

    modified

--- index.js
+++ index.js
@@ -17,27 +17,26 @@ app.set('view engine','pug')
 app.set('views', './views');
 v1ApiRouter.get('/', function (req, res) {
 		//进行数据库操作
-				db.collection('item',{safe:true}, function(err, collection){
-						if(err){
-						console.log(err);
-						}else{
-						var page = Number(req.query.page);
-						var nperpage = Number(req.query.nperpage);
+	db.collection('item',{safe:true}, function(err, collection){
+	if(err){
+		console.log(err);
+	}else{
+	 var page = Number(req.query.page);
+	 var nperpage = Number(req.query.nperpage);
 
-						console.log(page);
-						//分页功能，nperpage 是一页里面的记录数
-						//page 是第几页
-						//skip()跳了几条记录，limit(一页的记录数)		  
-						collection.find()
-						.skip((page-1)*nperpage) 
-						.limit(nperpage).toArray(function(err,results){
-								console.log(results);
+	 console.log(page);
+	//分页功能，nperpage 是一页里面的记录数
+	//page 是第几页
+	//skip()跳了几条记录，limit(一页的记录数)		  
+	collection.find()
+	.skip((page-1)*nperpage) 
+	.limit(nperpage)
+  .toArray(function(err,results){
+		   console.log(results);
+							              	});
 
-								});
-
-						res.render('index', { page: page, nperpage: nperpage })
-
-						db.close();
+		res.render('index', { page: page, nperpage: nperpage })
+				db.close();
 						}
 				});
 			
@@ -49,6 +48,6 @@ app.use('/v1',v1ApiRouter);
 
 app.listen(3000,function(){
 
-		console.log('port 3000')
+console.log('port 3000')
 })
 

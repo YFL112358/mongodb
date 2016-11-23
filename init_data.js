03a887dce9c237fa51e8ed36054a24d4f413b9c6
shareming7 <2395277764@qq.com>
2016-11-23 13:53:02 +0800

    modified init_data.js

--- init_data.js
+++ init_data.js
@@ -1,34 +1,34 @@
-
 var  mongodb = require('mongodb');
 var  server  = new mongodb.Server('localhost',27017, {auto_reconnect:true});
 var  db = new mongodb.Db('mypractice', server, {safe:true});
       function getRandomString() {   
       var chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';  
       var str = '';
-      
-          for (i = 0; i <(Math.random() * 26)+6 ; i++) {
+      //随机生成随机数
+      for (i = 0; i <(Math.random() * 26)+6 ; i++) {
           str += chars.charAt(Math.floor(Math.random()*26)+6);
-}
+                                                   }
       
       return str;
-  }
+                                }
   
       var a = getRandomString();
       console.log(a);
-
+    //连接数据库
 			db.open(function(err, db){
     if(!err){
         console.log('connect db');
-        // 连接Collection（可以认为是mysql的table）
          db.collection('item',{safe:true}, function(err, collection){
              if(err){
                  console.log(err);
              }else{
+						//插入数据库之前清空数据库
 										collection.remove();
+						//插入100条记录
                     for(var i = 0;i<100;i++){
                     collection.insert({ name: a+i, description: a+i,last_updated:new Date() }). then(console.log);}
 
                   }
-});
-   }
-}); 
+             });
+         } 
+  });   

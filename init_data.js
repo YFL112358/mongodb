646d976c83185d49338ab910d620c5c6f3bb02cb
shareming7 <2395277764@qq.com>
2016-11-25 09:52:48 +0800

    modified init_data.js

--- init_data.js
+++ init_data.js
@@ -1,34 +1,34 @@
 var  mongodb = require('mongodb');
 var  server  = new mongodb.Server('localhost',27017, {auto_reconnect:true});
 var  db = new mongodb.Db('mypractice', server, {safe:true});
-      function getRandomString() {   
-      var chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';  
-      var str = '';
+function getRandomString() {   
+	var chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';  
+	var str = '';
       //随机生成随机数
-      for (i = 0; i <(Math.random() * 26)+6 ; i++) {
-          str += chars.charAt(Math.floor(Math.random()*26)+6);
-                                                   }
-      
-      return str;
-                                }
+	for (i = 0; i <(Math.random() * 26)+6 ; i++) {
+		str += chars.charAt(Math.floor(Math.random()*26)+6);
+	} 
+	return str;
+}
   
-      var a = getRandomString();
-      console.log(a);
+var a = getRandomString();
+console.log(a);
     //连接数据库
-      db.open(function(err, db){
-      if(!err){
-        console.log('connect db');
-         db.collection('item',{safe:true}, function(err, collection){
-             if(err){
-                 console.log(err);
-             }else{
+db.open(function(err, db){
+	if(!err){
+		console.log('connect db');
+		db.collection('item',{safe:true}, function(err, collection){
+			if(err){
+				console.log(err);
+			} else {
 		 //插入数据库之前清空数据库
-		    collection.remove();
+				collection.remove();
 		//插入100条记录
-                    for(var i = 0;i<100;i++){
-                    collection.insert({ name: a+i, description: a+i,last_updated:new Date() }). then(console.log);}
-
-                  }
-             });
-         } 
-  });   
+				for(var i = 0;i<100;i++){
+					collection.insert({ name: a+i, description: a+i
+															,last_updated:new Date() })
+															. then(console.log);}
+				}
+		});
+	} 
+});   
